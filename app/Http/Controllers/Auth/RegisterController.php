6c63806429fd36717f5bfefbd3f855fcdbbd31e9
hakim-asrori <saneglos005@gmail.com>
2023-02-15 15:32:01 +0700

    membuat API register

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\Role as ModelsRole;
+use App\Repositories\User\UserRepository;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Spatie\Permission\Contracts\Role;
+
+class RegisterController extends Controller
+{
+    protected $userRepository;
+    protected $roleRepository;
+
+    public function __construct(UserRepository $userRepository, Role $roleRepository)
+    {
+        $this->userRepository = $userRepository;
+        $this->roleRepository = $roleRepository;
+    }
+
+    public function __invoke(RegisterRequest $request)
+    {
+        $request->merge([
+            'password' => bcrypt(Str::random(8))
+        ]);
+
+        $user = DB::transaction(function () use ($request) {
+            $role = $this->roleRepository->findById(ModelsRole::GUEST, 'api');
+            $user = $this->userRepository->create($request->all());
+
+            $user->assignRole($role);
+
+            return $user;
+        });
+
+        return response()->json([
+            'message' => 'Registrasi berhasil',
+            'data' => $user,
+            'status_code' => 201
+        ], 201);
+    }
+}
---
+++ routes/api/auth.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\Auth\RegisterController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('auth')->group(function () {
+    Route::post('/register', RegisterController::class)->name('api.auth.register');
+});
