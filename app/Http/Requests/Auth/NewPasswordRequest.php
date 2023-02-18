979b8c8436f19c4fccdd22a13cb89484b24aa101
hakim-asrori <saneglos005@gmail.com>
2023-02-18 12:54:55 +0700

    membuat fitur API New Password

---
+++ app/Http/Requests/Auth/NewPasswordRequest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
+
+class NewPasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'email' => ['required', 'email', Rule::exists('users', 'email'),],
+            'token' => 'required',
+            'password' => 'required|same:password_confirmation|min:8',
+            'password_confirmation' => 'required|same:password|min:8'
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages()
+    {
+        return [
+            'password.required' => 'password tidak boleh kosong',
+            'password.same' => 'password tidak sama',
+            'password_confirmation.required' => 'konfirmasi password tidak boleh kosong',
+            'password_confirmation.same' => 'konfirmasi password tidak sama'
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $response = new JsonResponse([
+            'messages' => $validator->errors(),
+            'status_code' => 400
+        ], 400);
+
+        throw new ValidationException($validator, $response);
+    }
+}
