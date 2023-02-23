4177abfed02ae3934b245bf2a91ce336500bc993
hakim-asrori <saneglos005@gmail.com>
2023-02-23 21:21:25 +0700

    membuat API expert skill

---
+++ app/Repositories/ExpertSkill/ExpertSkillRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories\ExpertSkill;
+
+use LaravelEasyRepository\Repository;
+
+interface ExpertSkillRepository extends Repository
+{
+    public function query();
+}
--- database/seeders/DatabaseTesterSeeder.php
+++ database/seeders/DatabaseTesterSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use Database\Seeders\Tests\ExpertSkillSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseTesterSeeder extends Seeder
@@ -17,6 +19,7 @@ class DatabaseTesterSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             RoleSeeder::class,
+            ExpertSkillSeeder::class
         ]);
     }
 }
