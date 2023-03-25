5ea11632c2aac862cabf6d9b5e45cd87fc67a03e
hakim-asrori <saneglos005@gmail.com>
2023-03-26 02:59:59 +0700

    perbaikan try catch di welder member

--- app/Http/Resources/User/WelderMemberCollection.php
+++ app/Http/Resources/User/WelderMemberCollection.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Resources\User;
 
+use App\Http\Traits\MessageFixer;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class WelderMemberCollection extends ResourceCollection
 {
+    use MessageFixer;
     /**
      * Transform the resource collection into an array.
      *
@@ -14,6 +16,6 @@ class WelderMemberCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return $this->collectionMessage();
     }
 }
