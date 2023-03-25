c0ff56a02c925baafa9dcbfc1107d484145cb168
hakim-asrori <saneglos005@gmail.com>
2023-03-26 02:12:21 +0700

    perbaikan try catch di article

--- app/Http/Resources/Article/ArticleCollection.php
+++ app/Http/Resources/Article/ArticleCollection.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Resources\Article;
 
+use App\Http\Traits\MessageFixer;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class ArticleCollection extends ResourceCollection
 {
+    use MessageFixer;
     /**
      * Transform the resource collection into an array.
      *
@@ -14,6 +16,6 @@ class ArticleCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return $this->collectionMessage();
     }
 }
