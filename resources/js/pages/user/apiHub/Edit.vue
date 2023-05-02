b7dc7332f21bf57d829ce8941160361076df595a
syarifhdyt009xd@gmail.com <syarifhdyt009xd@gmail.com>
2023-05-02 18:17:33 +0700

    Belum Selesai

--- resources/js/pages/user/apiHub/Edit.vue
+++ resources/js/pages/user/apiHub/Edit.vue
@@ -3,15 +3,43 @@ import PageTitle from "../../../components/PageTitle.vue";
 import Success from "../../../components/notifications/Success.vue";
 
 export default {
+    props: ["id"],
+    mounted(){
+
+    },
     data() {
         return {
-            msg: "",
+            user: {},
         };
     },
     methods: {
+        onCancel() {
+            this.$emit("onCancel", true);
+        },
+        getUser(){
+            this.$store
+            .dispatch("showData" ["user/hub", this.id])
+            .then((response) => {
+                this.user = response.data;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        },
         handleSubmit() {
-            $("#successModal").modal("show");
-            this.msg = "data berhasil disimpan.";
+            this.isLoading = true;
+            this.error = {};
+            this.$store
+            .dispatch("updateData", ["user/hub", this.id, this.user])
+            .then((response)=> {
+                this.isLoading = false;
+                    $("#successModal").modal("show");
+                    this.$emit("onCancel", true);
+            })
+            .catch((error)=> {
+                this.isLoading = false;
+                this.errors = error.response.data.messages;
+            });
         },
     },
     components: { PageTitle, Success },
@@ -21,44 +49,134 @@ export default {
     <PageTitle :title="'Edit Pengguna API Pusat'" />
 
     <div class="card">
-        <form @submit.prevent="handleSubmit">
+        <form @submit.prevent="handleSubmit" method="post">
             <div class="card-body">
                 <div class="mb-2">
                     <label>Nama Lengkap</label>
-                    <input type="text" class="form-control" value="John Doe" />
+                    <input 
+                    type="text"
+                    class="form-control"
+                    id="name"
+                    v-model="user.name"
+                    :class="{'is-invalid': errors.name}"
+                    :disabled="isLoading"
+                    />
+                    <div class="invalid-feedback"
+                        v-if="errors.name"
+                        v-for="(error, index) in errors.name"
+                        :key="index">
+                        {{ error }}.
+                    </div>
                 </div>
                 <div class="mb-2">
                     <label>Email</label>
-                    <input
-                        type="text"
-                        class="form-control"
-                        value="john.doe@mailinator.com"
+                    <input 
+                    type="text" 
+                    class="form-control"
+                    id="email"
+                    v-model="user.email"
+                    :class="{'is-invalid': errors.email}"
+                    :disabled="isLoading"
                     />
+                    <div class="invalid-feedback"
+                        v-if="errors.email"
+                        v-for="(error, index) in errors.email"
+                        :key="index">
+                        {{ error }}.
+                    </div>
+                </div>
+                <div class="mb-2">
+                    <label>Alamat</label>
+                    <input 
+                    type="text" 
+                    class="form-control"
+                    id="address"
+                    v-model="user.address"
+                    :class="{'is-invalid': errors.address}"
+                    :disabled="isLoading"
+                    />
+                    <div class="invalid-feedback"
+                        v-if="errors.address"
+                        v-for="(error, index) in errors.address"
+                        :key="index">
+                        {{ error }}.
+                    </div>
                 </div>
                 <div class="mb-2">
                     <label>Jabatan</label>
-                    <input
-                        type="text"
-                        class="form-control"
-                        value="Sekretaris"
+                    <input 
+                    type="text" 
+                    class="form-control"
+                    id="position"
+                    v-model="user.position"
+                    :class="{'is-invalid': errors.position}"
+                    :disabled="isLoading"
                     />
+                    <div class="invalid-feedback"
+                        v-if="errors.position"
+                        v-for="(error, index) in errors.position"
+                        :key="index">
+                        {{ error }}.
+                    </div>
                 </div>
                 <div class="mb-2">
                     <label>Telepon</label>
-                    <input
-                        type="text"
-                        class="form-control"
-                        value="081234567890"
+                    <input 
+                    type="text" 
+                    class="form-control"
+                    id="phone"
+                    v-model="user.phone"
+                    :class="{'is-invalid': errors.phone}"
+                    :disabled="isLoading"
                     />
+                    <div class="invalid-feedback"
+                        v-if="errors.phone"
+                        v-for="(error, index) in errors.phone"
+                        :key="index">
+                        {{ error }}.
+                    </div>
+                </div>
+                <div>
+                    <label>Status</label>
+                    <select
+                    class="form-select" 
+                    aria-label="Default select example"
+                    v-model="user.status"
+                    :class="{'is-invalid': errors.status}"
+                    :disabled="isLoading"
+                    >
+                            <option value="0">Aktif</option>
+                            <option value="1">Tidak Aktif</option>
+                    </select>
+                    <div class="invalid-feedback"
+                        v-if="errors.status"
+                        v-for="(error, index) in errors.status"
+                        :key="index">
+                        {{ error }}.
+                    </div>
                 </div>
             </div>
             <div class="card-footer border-top d-flex justify-content-between">
                 <router-link
                     :to="{ name: 'User Hub' }"
                     class="btn btn-secondary"
+                    :disabled="isLoading"
                     >Batal</router-link
                 >
-                <button class="btn btn-primary">Simpan</button>
+                <button class="btn btn-primary" v-if="!isLoading">Simpan</button>
+                <button
+                    class="btn btn-success"
+                    type="button"
+                    disabled
+                    v-if="isLoading"
+                >
+                    <span
+                        class="spinner-border spinner-border-sm me-1"
+                        role="status"
+                        aria-hidden="true"
+                    ></span>
+                    Harap Tunggu...
+                </button>
             </div>
         </form>
     </div>
--- resources/js/pages/user/apiHub/Index.vue
+++ resources/js/pages/user/apiHub/Index.vue
@@ -3,61 +3,115 @@ import Success from "../../../components/notifications/Success.vue";
 import Confirm from "../../../components/notifications/Confirm.vue";
 import PageTitle from "../../../components/PageTitle.vue";
 import Pagination from "../../../components/Pagination.vue";
+import Loader from "../../../components/Loader.vue";
+import CreateUser from "./Create.vue"
+import EditUser from "./Edit.vue"
 
 export default {
     data() {
         return {
-            users: [
-                {
-                    id: 1,
-                    name: "John Doe",
-                    email: "john.doe@mailinator.com",
-                    position: "Sekretaris",
-                    phone: "081234567890",
-                    status: 1,
-                },
-                {
-                    id: 2,
-                    name: "Jane Doe",
-                    email: "jane.doe@mailinator.com",
-                    position: "Kepala Cabang",
-                    phone: "081234567890",
-                    status: 0,
-                },
-                {
-                    id: 3,
-                    name: "Hana Doe",
-                    email: "hana.doe@mailinator.com",
-                    position: "Kepala Pusat",
-                    phone: "081234567890",
-                    status: 1,
-                },
-            ],
+            id : null,
+            title: "Daftar Pengguna API Pusat",
+            isCreate: false,
+            isEdit: false,
+            users: [],
             msg: "",
+            pagination: {
+                perPage: 10,
+                page: 1,
+            },
+            filters: {
+                search: ""
+            },
+            
         };
     },
+    mounted(){
+        this.getUsers();
+    },
     methods: {
-        handleDelete() {
+        getUsers(){
+            this.isLoading = true;
+            let params = [
+                `search=${this.filters.search}`
+            ].join("&");
+
+            this.$store
+            .dispatch("getData",["user/hub", params])
+            .then((response)=>{
+                this.isLoading = false;
+                this.users = response.data;
+            })
+            .catch((error) => {
+                this.isLoading = false;
+                console.log( error);
+            })
+        },
+        handleDelete(id) {
+            this.id = id;
             $("#confirmModal").modal("show");
         },
-        onDelete() {
-            $("#confirmModal").modal("hide");
-            $("#successModal").modal("show");
-            this.msg = "data berhasil dihapus.";
+        onCreate() {
+            this.title = "Tambah Pengguna";
+            this.isCreate = true;
         },
+        onEdit(id) {
+            this.title = "Edit Data Pengguna";
+            this.id = id;
+            this.isEdit = true;
+        },
+        onCancel() {
+            this.title = "Daftar Pengguna API Pusat";
+            this.isCreate = false;
+            this.isEdit = false;
+        },
+        onDelete() {
+            this.$store
+                .dispatch("deleteData", ["user/hub", this.id])
+                .then((response) => {
+                    $("#confirmModal").modal("hide");
+                    this.showAlert = true;
+                    this.msg = "data berhasil dihapus.";
+                    this.getUsers();
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        },
+        
         onUpdateStatus(id, status) {
             $("#successModal").modal("show");
             this.msg = `status berhasil diperbaharui.`;
         },
+        onSearch() {
+            this.getUsers();
+        },
+        onPageChange(e) {
+            this.pagination.page = e;
+            this.getUsers();
+        },
     },
-    components: { Pagination, PageTitle, Success, Confirm },
+    components: { 
+        Pagination,
+        PageTitle,  
+        Success, 
+        Confirm,
+        CreateUser,
+        EditUser,
+        Loader
+        },
 };
 </script>
 <template>
-    <PageTitle :title="'Daftar Pengguna API Pusat'" />
+    <PageTitle :title="title" />
 
-    <div class="card">
-        <div class="card-body">
+    <CreateUser v-if="isCreate" @onCancel="onCancel($e)" />
+
+    <EditUser v-else-if="isEdit" :id="id" @onCancel="onCancel($e)" />
+
+    <div v-else class="card">
+        <div class="card-body position-relative">
+            <Loader v-if="isLoading" />
             <div class="table-responsive">
                 <div
                     class="d-md-flex d-block justify-content-between align-items-center mb-2"
@@ -78,6 +132,8 @@ export default {
                                 type="search"
                                 class="form-control"
                                 placeholder="pencarian"
+                                @keyup="onSearch"
+                                v-model="filters.search"
                             />
                         </div>
 
@@ -93,6 +149,7 @@ export default {
                             <th>No.</th>
                             <th>Nama Pengguna</th>
                             <th>Email Pengguna</th>
+                            <th>Alamat</th>
                             <th>Jabatan</th>
                             <th>Telepon</th>
                             <th>Status</th>
@@ -104,6 +161,7 @@ export default {
                             <th v-html="index + 1"></th>
                             <td v-html="user.name"></td>
                             <td v-html="user.email"></td>
+                            <td v-html="user.address"></td>
                             <td v-html="user.position"></td>
                             <td v-html="user.phone"></td>
                             <td>
