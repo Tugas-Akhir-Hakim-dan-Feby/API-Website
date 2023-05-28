4972efbf02c46a9eaaeaf58230527af31dbb2f85
hakim-asrori <saneglos005@gmail.com>
2023-05-28 14:58:09 +0700

    crud company member complete

---
+++ app/Http/Filters/User/CompanyMember/Search.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Filters\User\CompanyMember;
+
+use Closure;
+use Illuminate\Database\Eloquent\Builder;
+
+class Search
+{
+    public function handle(Builder $query, Closure $next)
+    {
+        if (!request()->has('search')) {
+            return $next($query);
+        }
+        $query->where(function ($query) {
+            $query->where('name', 'like', "%" . request('search') . "%")
+                ->orWhere('email', 'like', "%" . request('search') . "%")
+                ->orWhereHas('companyMember', function ($query) {
+                    $query->where('company_name', 'like', "%" . request('search') . "%");
+                });
+        });
+
+        return $next($query);
+    }
+}
--- resources/js/pages/user/company/Index.vue
+++ resources/js/pages/user/company/Index.vue
@@ -3,84 +3,100 @@ import Success from "../../../components/notifications/Success.vue";
 import Confirm from "../../../components/notifications/Confirm.vue";
 import PageTitle from "../../../components/PageTitle.vue";
 import Pagination from "../../../components/Pagination.vue";
+import Loader from "../../../components/Loader.vue";
 
 export default {
     data() {
         return {
-            users: [
-                {
-                    id: 1,
-                    companyName: "PT. ABC",
-                    name: "John Doe",
-                    email: "john.doe@mailinator.com",
-                    phone: "081234567890",
-                    status: 1,
-                },
-                {
-                    id: 2,
-                    companyName: "PT. CDE",
-                    name: "Jane Doe",
-                    email: "jane.doe@mailinator.com",
-                    phone: "081234567890",
-                    status: 0,
-                },
-                {
-                    id: 3,
-                    companyName: "PT. FGH",
-                    name: "Nana Doe",
-                    email: "nana.doe@mailinator.com",
-                    phone: "081234567890",
-                    status: 0,
-                },
-            ],
+            users: [],
+            pagination: {
+                perPage: 10,
+                page: 1,
+            },
+            filters: {
+                search: "",
+            },
+            metaPagination: {},
             msg: "",
+            isLoading: false,
         };
     },
+    mounted() {
+        this.getUsers();
+    },
     methods: {
+        getUsers() {
+            this.isLoading = true;
+            let params = [
+                `per_page=${this.pagination.perPage}`,
+                `page=${this.pagination.page}`,
+                `search=${this.filters.search}`,
+            ].join("&");
+
+            this.$store
+                .dispatch("getData", ["user/company-member", params])
+                .then((response) => {
+                    this.isLoading = false;
+                    this.users = response.data;
+                    this.metaPagination = response.meta;
+                })
+                .catch((error) => {
+                    this.isLoading = false;
+                    console.log(error);
+                });
+        },
         handleDelete() {
             $("#confirmModal").modal("show");
+        },
+        onSearch() {
+            setTimeout(() => {
+                this.getUsers();
+            }, 1000);
         },
         onDelete() {
             $("#confirmModal").modal("hide");
             $("#successModal").modal("show");
             this.msg = "data berhasil dihapus.";
         },
+        onPageChange(e) {
+            this.pagination.page = e;
+            this.getUsers();
+        },
         onUpdateStatus(id, status) {
             $("#successModal").modal("show");
             this.msg = `status berhasil diperbaharui.`;
         },
     },
-    components: { Pagination, PageTitle, Success, Confirm },
+    components: { Pagination, PageTitle, Success, Confirm, Loader },
 };
 </script>
 <template>
     <PageTitle :title="'Daftar Pengguna Member Perusahaan'" />
 
     <div class="card">
+        <Loader v-if="isLoading" />
         <div class="card-body">
             <div class="table-responsive">
                 <div
                     class="d-md-flex d-block justify-content-between align-items-center mb-2"
                 >
                     <div class="text-center">
-                        <!-- <router-link
-                                :to="{ name: 'User Hub Create' }"
-                                class="btn btn-primary mb-2"
-                                >Tambah Pengguna</router-link
-                            > -->
                         <input
                             type="search"
                             class="form-control"
                             placeholder="pencarian"
+                            v-model="filters.search"
+                            @input="onSearch"
                         />
                     </div>
 
                     <div
                         class="d-md-flex justify-content-between align-items-center"
                     >
-                        <!-- <div class="me-md-2 me-0"></div> -->
-
-                        <Pagination />
+                        <Pagination
+                            :pagination="metaPagination"
+                            @onPageChange="onPageChange($event)"
+                        />
                     </div>
                 </div>
             </div>
@@ -93,46 +109,24 @@ export default {
                             <th>Nama Perusahaan</th>
                             <th>Nama Pengguna</th>
                             <th>Email</th>
-                            <th>Telepon</th>
-                            <th>Status</th>
                             <th>Aksi</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr v-for="(user, index) in users" :key="index">
                             <th v-html="index + 1"></th>
-                            <td v-html="user.companyName"></td>
+                            <td v-html="user.companyMember?.companyName"></td>
                             <td v-html="user.name"></td>
                             <td v-html="user.email"></td>
-                            <td v-html="user.phone"></td>
-                            <td>
-                                <div class="form-check form-switch">
-                                    <input
-                                        class="form-check-input"
-                                        type="checkbox"
-                                        role="switch"
-                                        :checked="user.status"
-                                        @click="
-                                            onUpdateStatus(user.id, user.status)
-                                        "
-                                    />
-                                </div>
-                            </td>
                             <td>
                                 <router-link
                                     :to="{
-                                        name: 'User Hub Edit',
-                                        params: { id: user.id },
+                                        name: 'User Company Show',
+                                        params: { id: user.uuid },
                                     }"
-                                    class="btn btn-sm btn-warning me-2"
-                                    >Edit</router-link
+                                    class="btn btn-sm btn-info me-2"
+                                    >Detail</router-link
                                 >
-                                <button
-                                    @click="handleDelete"
-                                    class="btn btn-sm btn-danger"
-                                >
-                                    Hapus
-                                </button>
                             </td>
                         </tr>
                     </tbody>
