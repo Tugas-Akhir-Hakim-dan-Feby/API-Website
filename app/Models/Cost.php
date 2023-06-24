b05f0be6d7c0ad340c3d349f031137be7554f24f
hakim-asrori <saneglos005@gmail.com>
2023-06-24 13:26:02 +0700

    variable const in cost model

--- app/Models/Cost.php
+++ app/Models/Cost.php
@@ -9,6 +9,14 @@ class Cost extends Model
 {
     use HasFactory;
 
+    const WELDER_MEMBER = 1;
+
+    const COMPANY_MEMBER = 2;
+
+    const ADVERTISEMENT = 3;
+
+    const TEST_INSTITUTION = 4;
+
     protected $fillable = [
         'type_price',
         'nominal_price',
