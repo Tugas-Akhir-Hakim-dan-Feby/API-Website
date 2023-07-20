a5a335dd3fdff76552ecaa1d2d70afc40ce5cdfe
hakim-asrori <saneglos005@gmail.com>
2023-07-21 01:16:58 +0700

    add max date in admin hub

--- resources/js/pages/user/profile/ProfileAdminHub.vue
+++ resources/js/pages/user/profile/ProfileAdminHub.vue
@@ -10,6 +10,17 @@ export default {
             isLoading: false,
         };
     },
+    computed: {
+        maxDateBirth() {
+            let date = new Date();
+            date.setFullYear(date.getFullYear() - 17);
+            date.setMonth(date.getMonth());
+            date.setDate(date.getDate());
+
+            let formattedDate = date.toISOString().split("T")[0];
+            return formattedDate;
+        },
+    },
     mounted() {
         this.getUser();
     },
@@ -225,6 +236,7 @@ export default {
                                     :class="{
                                         'is-invalid': errors.dateBirth,
                                     }"
+                                    :max="maxDateBirth"
                                 />
                                 <div
                                     class="invalid-feedback"
